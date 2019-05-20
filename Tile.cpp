13b8b0353fca8b992ef5d04e70eea61ffeea88c0
bowen zhang <s3617571@users.noreply.github.com>
2019-05-21 00:02:38 +1000

    revert

--- Tile.cpp
+++ Tile.cpp
@@ -21,6 +21,30 @@ std::string Tile::print() {
     char temp[] = {colour, '\0'};
     std::string B = std::to_string(shape);
     std::string str = temp + B;
+    if(colour == 'R') {
+        std::string tc = RED + str + WHITE;
+        str = tc;
+    }
+    else if(colour == 'G') {
+        std::string tc = GREEN + str + WHITE;
+        str = tc;
+    }
+    else if(colour == 'Y') {
+        std::string tc = YELLOW + str + WHITE;
+        str = tc;
+    }
+    else if(colour == 'B') {
+        std::string tc = BLUE + str + WHITE;
+        str = tc;
+    }
+    else if(colour == 'P') {
+        std::string tc = MAGENTA + str + WHITE;
+        str = tc;
+    }
+    else if(colour == 'O') {
+        std::string tc = ORANGE + str + WHITE;
+        str = tc;
+    }
     return str;
 }
 
