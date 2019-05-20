1bb062ef11516cbb76b595fe7b7107bf79d62274
bowen zhang <s3617571@users.noreply.github.com>
2019-05-20 23:56:34 +1000

    Revert "test"
    This reverts commit 66a0eebf669272d83611e25f8e6caf21f4a11435.

--- Qwirkle.h
+++ Qwirkle.h
@@ -1,18 +1,11 @@
 #ifndef ASSIGN2_QWIRKLE_H
 #define ASSIGN2_QWIRKLE_H
 
-#include "LinkedList.h"
-#include "Player.h"
-#include "TileCodes.h"
-#include "Board.h"
-
 #include <iostream>
 #include <fstream>
 #include <regex>
 
-
 class Qwirkle {
-
 public:
     Qwirkle();
     ~Qwirkle();
@@ -26,10 +19,19 @@ public:
 
     //secondery functions
     void initialBag();
+    void initialMap();
+    void paintMap();
+    void placeTile(std::vector<Tile> tile, std::vector<std::string> location);
     void play();
+    void extendMap(unsigned int mapSize);
     void helpCommand();
     bool placeAtCommand(std::string tiles, std::string location);
     bool replaceCommand(std::string tiles);
+    bool isValid(std::vector<Tile>& tile, std::vector<std::string> location);
+    bool isColValid(Tile& tile, const unsigned int& x, const unsigned int& y);
+    bool isRowValid(Tile& tile, const unsigned int& x, const unsigned int& y);
+    int calculateColScore(Tile& tile, const unsigned int& x, const unsigned int& y);
+    int calculateRowScore(Tile& tile, const unsigned int& x, const unsigned int& y);
     //helper fuctions
     void gameOver();
     void switchTurn();
@@ -48,8 +50,7 @@ private:
     bool isFirstTile = true;
     bool gamePlay = true;
     Player* player;
-    Board* board;
-
+    std::vector< std::vector<Tile> > gameMap;
 };
 
 #endif // ASSIGN2_QWIRKLE_H
