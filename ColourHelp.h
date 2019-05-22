9724f62c71205731032379bcb2a245be9dfc87cf
bowen zhang <s3617571@users.noreply.github.com>
2019-05-22 17:07:46 +1000

    test

---
+++ ColourHelp.h
@@ -0,0 +1,12 @@
+#define RESET   "\033[0m"      /* RESET */
+#define RColour "\033[31m"      /* Red */
+#define GColour "\033[32m"      /* Green */
+#define WColour "\033[37m"      /* White */
+#define BColour "\033[34m"      /* Blue */
+#define Cyan    "\033[36m"      /* Cyan */
+
+#define BUR     "\033[31;1;4m"  /*BOLD UNDERLINE RED*/
+#define BUC     "\033[36;1;4m"  /*BOLD UNDERLINE Cyan*/
+#define BUG     "\033[32;1;4m"  /*BOLD UNDERLINE Green*/
+#define BUM     "\033[35;1;4m"  /*BOLD UNDERLINE Magenta*/
+#define BUY     "\033[93;1;4m"  /*BOLD UNDERLINE Yellow*/
